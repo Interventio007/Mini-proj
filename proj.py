683a1998cf5a9c0ca688798397e1f48f3ac9b640
Interventio007 <42114664+Interventio007@users.noreply.github.com>
2018-08-19 23:18:59 +0530

    proj

---
+++ proj.py
@@ -0,0 +1,220 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem, QPlainTextEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+import datetime
+
+
+class Window(QtWidgets.QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.name()
+        
+
+    def name(self):
+
+        self.windows = QtWidgets.QWidget()
+
+        self.l_name = QtWidgets.QLabel(self.windows)
+        self.l_name.setText("Name")
+        
+        self.e_name = QtWidgets.QLineEdit(self.windows)
+
+        self.b_ok = QtWidgets.QPushButton("OK",self.windows)
+
+        self.b_ok.move(220,70)
+        
+        self.l_name.move(50,35)
+
+        self.e_name.move(100,35)
+
+        self.windows.setGeometry(500,400,300,100)
+
+        self.windows.show()
+
+        self.b_ok.clicked.connect(self.ok_click)
+
+    def phone(self):
+
+        self.window_phone = QtWidgets.QWidget()
+
+        self.l_phone_name = QtWidgets.QLabel(self.window_phone)
+        self.l_phone_name.setText("Name")
+        
+        self.e_phone_name = QtWidgets.QLineEdit(self.window_phone)
+
+        self.b_ok_1 = QtWidgets.QPushButton("OK",self.window_phone)
+
+        self.b_ok_1.move(220,70)
+
+        self.l_phone_name.move(50,40)
+
+        self.e_phone_name.move(100,40)
+
+        self.window_phone.setGeometry(500,400,300,100)
+
+        self.window_phone.show()
+
+        self.b_ok_1.clicked.connect(self.ok_click_phone)
+       
+
+
+
+    def address(self):
+        
+        self.windows_1 = QtWidgets.QWidget()
+
+        self.L1_name = QtWidgets.QLabel(self.windows_1)
+        self.L1_name.setText("Name")
+        
+        self.E1_name = QtWidgets.QLineEdit(self.windows_1)
+
+        self.b_ok_2 = QtWidgets.QPushButton("OK",self.window_phone)
+
+        self.b_ok_2.move(220,70)
+
+        self.L1_name.move(50,40)
+
+        self.E1_name.move(100,40)
+
+        self.windows_1.setGeometry(500,400,300,100)
+
+        self.windows_1.show()
+
+        self.b_ok_1.clicked.connect(self.ok_click_address)
+
+       
+
+
+    def ok_click(self):
+
+        self.phone()
+
+        self.window_phone.close()
+
+    def ok_click_phone(self):
+
+        self.address()
+
+        self.windows.close()
+
+    def ok_click_address(self):
+
+        self.initUI()
+
+        self.windows_1.close()
+    
+        
+        
+    def initUI(self):
+        
+
+        self.w = QtWidgets.QWidget()
+        self.w.setGeometry(0,0,1920,1080)
+        self.w.setWindowTitle("GreenBill")
+
+        self.l1_image = QtWidgets.QLabel(self.w)
+        self.l1_title = QtWidgets.QLabel(self.w)
+
+        self.l1_image.setPixmap(QtGui.QPixmap("/Users/srinivas/Downloads/gg.png"))
+        self.l1_title.setText("Green Bill")
+
+        self.setStyleSheet("QtLabel {font: 50pt Roboto}")
+        self.l1_title.setStyleSheet("font: 50pt Roboto")
+          
+        self.l1_image.move(50,25)
+        self.l1_title.move(125,25)
+
+        self.l1_bill = QtWidgets.QLabel(self.w)
+        self.l1_name = QtWidgets.QLabel(self.w)
+        self.l1_phone = QtWidgets.QLabel(self.w)
+
+        self.l1_bill.setStyleSheet("font: 20pt Roboto")
+        self.l1_name.setStyleSheet("font: 20pt Roboto")
+        self.l1_phone.setStyleSheet("font: 20pt Roboto")
+
+        self.l1_bill.setText("Bill No")
+        self.l1_name.setText("Name")
+        self.l1_phone.setText("Phone")
+
+        self.e1_bill = QtWidgets.QLineEdit(self.w)
+        self.e1_name = QtWidgets.QLineEdit(self.w)
+        self.e1_phone = QtWidgets.QLineEdit(self.w)
+
+        self.e1_name.setText("{0}".format(self.e_name.text()))
+
+        self.l1_bill.move(50,150)
+        self.l1_name.move(50,200)
+        self.l1_phone.move(50,250)
+
+        self.e1_bill.move(125,150)
+        self.e1_name.move(125,200)
+        self.e1_phone.move(125,250)
+
+        self.e1_bill.resize(200,25)
+        self.e1_name.resize(200,25)
+        self.e1_phone.resize(200,25)
+
+        self.l2_date = QtWidgets.QLabel(self.w)
+        self.l2_address = QtWidgets.QLabel(self.w)
+
+        self.e2_date = QtWidgets.QLineEdit(self.w)
+        self.e2_address = QtWidgets.QPlainTextEdit(self.w)
+    
+        self.l2_date.setText("Date")
+        self.l2_address.setText("Address")
+
+        self.l2_date.setStyleSheet("font: 20pt Roboto")
+        self.l2_address.setStyleSheet("font: 20pt Roboto")
+
+        self.l2_date.move(400,150)
+        self.l2_address.move(400,200)
+
+        self.e2_date.move(500,150)
+        self.e2_address.move(500,200)
+
+        self.e2_date.resize(200,25)
+        self.e2_address.resize(300,75)
+
+        self.tableWidget = QTableWidget(self.w)
+        self.tableWidget.setRowCount(1)
+        self.tableWidget.setColumnCount(10)
+        self.tableWidget.setSpan(0,0,1,3)
+        
+
+        self.tableWidget.move(50,300)
+        self.tableWidget.resize(1325,400)
+
+        self.b_save = QtWidgets.QPushButton("Save",self.w)
+        self.b_print = QtWidgets.QPushButton("Print",self.w)
+
+
+        self.b_save.move(1150,725)
+        self.b_print.move(1275,725)
+
+        self.b_save.resize(100,50)
+        self.b_print.resize(100,50)
+
+
+        time = datetime.datetime.now()
+        day = time.day
+        month = time.month
+        year = time.year
+
+        self.e2_date.setText("{0}/{1}/{2} ".format(day,month,year))
+
+        self.w.show()
+        
+        gg = self.name()
+        print(gg)
+    
+    
+    
+
+
+    
+app = QtWidgets.QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec_())
