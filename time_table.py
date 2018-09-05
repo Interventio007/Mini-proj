ea6f31c60e3b9295f8c08e25e1ebf862dba59d12
Sukhoi007 <42114664+Sukhoi007@users.noreply.github.com>
2018-09-05 23:19:13 +0530

    Update time_table.py

--- time_table.py
+++ time_table.py
@@ -12,6 +12,8 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.rowcount()
+        self.database_create()
+        self.database_table()
 
     def initUI(self):
 
@@ -30,24 +32,83 @@ class Window(QtWidgets.QWidget):
         self.tableWidget.verticalHeader().hide()
 
         self.resolution = QtWidgets.QDesktopWidget().screenGeometry()
-        self.width = (self.resolution.width() / 2) - 500
-        self.res_width = (self.frameSize().width() / 2) 
+        self.width = (self.resolution.width() / 2) - 650
+        self.height = (self.resolution.height() / 2) - 250
 
-        print(self.width)
-        print(self.res_width)
+        self.tableWidget.move(self.width,self.height ) 
 
-        self.tableWidget.move(self.width , (self.resolution.height() / 2) - (self.frameSize().height() / 2) ) 
-
-
-        self.tableWidget.resize(1000,500)
+        self.tableWidget.setColumnWidth(0,125)
+        self.tableWidget.setColumnWidth(1,125)
+        self.tableWidget.setColumnWidth(2,125)
+        self.tableWidget.setColumnWidth(3,150)
+        self.tableWidget.setColumnWidth(4,125)
+        self.tableWidget.setColumnWidth(5,125)
+        self.tableWidget.setColumnWidth(6,150)
+        self.tableWidget.setColumnWidth(7,125)
+        self.tableWidget.setColumnWidth(8,125)
+        self.tableWidget.setColumnWidth(9,125)
+        
+        self.tableWidget.setRowHeight(0,50)
+        self.tableWidget.setRowHeight(1,50)
+        self.tableWidget.setRowHeight(2,50)
+        self.tableWidget.setRowHeight(3,50)
+        self.tableWidget.setRowHeight(4,50)
+        self.tableWidget.setRowHeight(5,50)
+        self.tableWidget.setRowHeight(6,50)
+        self.tableWidget.setRowHeight(7,50)
+        self.tableWidget.setRowHeight(8,50)
+        self.tableWidget.setRowHeight(9,50)
+        
+        self.tableWidget.resize(1302,502)
 
         self.mainWindow.show()
+
+
+    def database_create(self):
+
+        self.mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        buffered = True
+        )
+
+        self.mycursor = self.mydb.cursor()
+        self.mycursor.execute("SHOW DATABASES")
+        self.db_exists = False
+        
+        for x in self.mycursor:
+
+            if x[0] == 'Time_Table':
+                self.db_exists = True
+        
+        if self.db_exists == False:
+            self.mycursor.execute("CREATE DATABASE Time_Table")
+            
+
+    def database_table(self):
+        
+        self.mycursor.execute("use Time_Table")
+        self.mycursor.execute("show tables")
+        self.table_exists = False
+
+        for a in self.mycursor:
+            if a[0] == 'Time_Day':
+                self.table_exists = True
+        
+        if self.table_exists == False:
+            self.mycursor.execute("Create Table Time_Day(Time Varchar(10), Day Varchar(10))")
     
 
     def rowcount(self):
 
         self.rowWindow = QtWidgets.QWidget()
-        self.rowWindow.setGeometry(650,400,310,100)
+        
+        self.resolution = QtWidgets.QDesktopWidget().screenGeometry()
+        self.window_1_width = (self.resolution.width() / 2) - 150
+        self.window_1_height = (self.resolution.height() / 2) - 50
+
+        self.rowWindow.setGeometry(self.window_1_width,self.window_1_height,310,100)
 
         self.row_label = QtWidgets.QLabel(self.rowWindow)
         self.row_label.setText("Enter row size")
@@ -70,7 +131,12 @@ class Window(QtWidgets.QWidget):
     def columncount(self):
         
         self.columnWindow = QtWidgets.QWidget()
-        self.columnWindow.setGeometry(650,400,350,100)
+        
+        self.resolution = QtWidgets.QDesktopWidget().screenGeometry()
+        self.window_2_width = (self.resolution.width() / 2) - 175
+        self.window_2_height = (self.resolution.height() / 2) - 50
+        
+        self.columnWindow.setGeometry(self.window_2_width,self.window_2_height,350,100)
 
         self.column_label = QtWidgets.QLabel(self.columnWindow)
         self.column_label.setText("Enter column size")
